e1a7f02d76201efdeb35d27f462fa6207e6e0e7f
LMX <2891632957@qq.com>
2026-08-12 21:24:06 +0800

    feat(i18n): 配置中英 ARB 并生成本地化代码

---
+++ lib/app/l10n/app_localizations_zh.dart
@@ -0,0 +1,16 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Chinese (`zh`).
+class AppLocalizationsZh extends AppLocalizations {
+  AppLocalizationsZh([String locale = 'zh']) : super(locale);
+
+  @override
+  String get appTitle => '我爱背单词';
+
+  @override
+  String get homeSkeletonReady => '工程骨架已就绪。';
+}
