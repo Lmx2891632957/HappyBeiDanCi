37ea0944dc36a17f46f1c45011edc3e2b7da9e72
LMX <2891632957@qq.com>
2026-08-12 21:25:12 +0800

    test(db): 移除数据库测试中未使用的 import

--- test/integration/database_schema_test.dart
+++ test/integration/database_schema_test.dart
@@ -1,6 +1,5 @@
 import 'dart:io';
 
-import 'package:drift/drift.dart';
 import 'package:drift/native.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:happy_bei_dan_ci/data/local/app_database.dart';
